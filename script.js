0e7ca7210e13103a690156cea76e29597670c284
Yashraaj Sharad Gorde <149304566+yashraj-gorde@users.noreply.github.com>
2025-09-26 17:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// Select all squares
+const squares = document.querySelectorAll(".square");
+
+// Colors
+const lavender = "#E6E6FA";
+const coffee = "#6F4E37";
+
+// Loop through each square
+squares.forEach((sq) => {
+  // When mouse enters one square
+  sq.addEventListener("mouseover", () => {
+    squares.forEach((otherSq) => {
+      if (otherSq !== sq) {
+        otherSq.style.backgroundColor = coffee;
+      }
+    });
+  });
+
+  // When mouse leaves that square
+  sq.addEventListener("mouseout", () => {
+    squares.forEach((s) => {
+      s.style.backgroundColor = lavender;
+    });
+  });
+});
